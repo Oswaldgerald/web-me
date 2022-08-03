cc2b8a83841a784f4cbde7e24aa2424e448e0dda
Oswald Gerald <moswaldgerald@gmail.com>
2022-08-03 11:59:49 +0300

    Initial filesfor first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
+import Header from './components/Header/Header';
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+      <Hero/>
+      {/* <Header/> */}
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import Header from '../Header/Header'
+import './Hero.css'
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+          <div className="left-h">
+            <Header/>
+            {/* the best ad */}
+            <div className="the-best-ad">
+              <div></div>
+              <span>Veni vidi vinci : Gadi Bullah</span>
+            </div>
+            {/* hero heading */}
+            <div className="hero-text">
+              <div>
+                <span>Fix </span>
+                <span className='stroke-text'>the cause,</span>
+              </div>
+              <div>
+                <span>
+                  Not the Symptom
+                </span>
+              </div>
+              <div>
+                <span>
+                Perfection is achieved not when there is nothing more to add, but rather when there is nothing more to take away.
+                </span>
+              </div>
+            </div>
+            {/* figures */}
+            <div className="figures">
+              <div>
+                <span>+140</span>
+                <span>Projects</span>
+              </div>
+              <div>
+                <span>+100</span>
+                <span>Dreams</span>
+              </div>
+              <div>
+                <span>+50</span>
+                <span>IDEs</span>
+              </div>
+            </div>
+            <div className="hero-buttons">
+              <buttons className="btn">Get Started</buttons>
+              <buttons className="btn">Learn More</buttons>
+            </div>
+          </div>
+      <div className="right-side">
+        <button className="btn">Join me</button>
+        <div className="heart-rate">
+          <img src={Heart}/>
+          <span>Heart rate</span>
+          <span>bpm</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Hero
